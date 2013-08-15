dca2ca95049e0e0e63e2969b2ed62dffb8c235da
Patrick Egan Foley <patrick.egan.foley@gmail.com>
2013-08-15 14:51:18 -0400

    I wrote the basis of a decent julia implementation.  It's in globesFunctions and globesScript.  I still need the function from spatial coordinates to spherical coordinates.  And the script is still just a skeleton.

---
+++ julia/globesFunctions.jl
@@ -0,0 +1,104 @@
+#  This file contains the following functions that make globe coordinate transfromations
+#  and pixel computations.
+#
+#    coordinateRotationFromXYZ
+#    rotationMatrix
+
+
+
+function longitudeAndLatitudeFromGallPetersPixels(pixels, image_size)
+
+    #  image_size is nx, ny.
+    (nx, ny) = image_size
+    #  This computes longitude and latitude for a given pixel value pair.
+
+    longitude = ((pixels[:,1]* (2*pi/nx)) + pi) % (2*pi)
+    latitude = 2 * asin( (2*pixels[:,2]/ny) -1)
+
+    return([longitude latitude])
+
+end
+
+
+
+function gallPetersPixels([longitude, latitude], image_size)
+
+    (nx, ny) = image_size
+
+    #  longitudes are from 0 to 2 pi.
+    #  lats are -pi/2 to pi/2.
+
+    u =  int( longitude * (nx/(2*pi)))
+    v =  int( (ny/2)* (sin(latitude)+1))
+
+    return [u v]
+
+end
+
+
+
+function spatialCoordinatesFromLongitudeAndLatitude([longitude, latitude])
+
+    #  This returns a row matrix (n_pixel rows, 3 columns) with 
+    #  positions in three dimensional space.
+
+    x = cos(longitude)*cos(latitude)
+    y = sin(longitude)*cos(latitude)
+    z = sin(latitude)
+
+    return [x y z]
+
+end
+
+
+
+function longitudeAndLatitudeFromSpatialCoordinates( [x y z] )
+
+    latitude = asin(z)
+
+    longitude = 
+
+
+
+end
+
+
+
+
+
+function coordinateRotationFromXYZ(angleX, angleY, angleZ)
+
+    Rx = rotationMatrix([1 0 0], angleX)
+    Ry = rotationMatrix([0 1 0], angleY)
+    Rz = rotationMatrix([0 0 1], angleZ)
+
+    return Rx * Ry * Rz
+
+end
+
+
+
+
+
+function rotationMatrix(axisVector, angle)
+
+    #  First check if the vector is a unit vector.
+    axisVector /= sqrt(sum(axisVector.^2))
+    (x, y, z) = axisVector
+
+
+    #  Now just plug in values from
+    #  en.wikipedia.org/wiki/Rotation_matrix#Roataion matrix from axis and angle
+    tensor_product_matrix = [ x^2     x*y    x*z ;
+                             x*y     y^2    y*z ;
+                             x*z     y*z    z^2 ]
+    cross_product_matrix = [ 0    -z    y ;
+                             z     0   -x ;
+                            -y     x    0 ]
+    R = eye(3)*cos(angle) + 
+        sin(angle)*cross_product_matrix + 
+        (1-cos(angle))*tensor_product_matrix
+
+    return R
+
+end
---
+++ julia/globesScript.jl
@@ -0,0 +1,7 @@
+#  This script uses the functions in globesFunctions to:
+#
+#     Read in an image.
+#     Compute longitude and latitude.
+#     Rotate the globe.
+#     Recompute pixel values
+#     Get a new image with reoriented pixels.
