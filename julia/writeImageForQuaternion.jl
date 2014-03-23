b1cd2b1eb45f6d9e106de3ea4187d94d1b593efd
Patrick Egan Foley <Patrick.Egan.Foley@gmail.com>
2014-03-22 20:22:44 -0400

    Whitespce on writeImageForQ

--- julia/writeImageForQuaternion.jl
+++ julia/writeImageForQuaternion.jl
@@ -31,65 +31,65 @@ function writeImageForQuaternion(q::quaternion, originalImage, newName)
     assignable_pixel_index = 1
     #  Now loop through the image in the order in which you will assign values.
     for y_index = 1:ny
-    
-    #  Do just the y stuff here.  
-    φ = asin( (2 * y_index / ny)  - 1 )    
-    
-    for x_index = 1:nx
-    
-    #  At each loop, you want to apply the full transformation to get the new_x and new_y.
-    #  Then put the correct pixel value in each color.  
-    
-    
-    #  Assign longitude.
-    λ = (2*pi) * ((x_index / nx) - (1/2))
-    
-    newLocation = rotateByQuaternion(SphCoords(φ, λ), q)
-    newφ = newLocation.φ
-    newλ = newLocation.λ
-    
-    #  Adjust to ensure longitudes and latitudes are in the appropriate ranges.
-    if newλ > pi
-        newλ = newλ - (2*pi)
-    end
-    
-    
-    #  Obtain new pixel locations
-    #new_x = int(R*new_longitude)
-    #new_y = int(γ * R * sin(new_latitude))
-    new_x = int( nx * ((1/2) + (newλ/(2*pi))))
-    new_y = int( ny * (1/2) * (1+sin(newφ)))
-    
-    #  Ensure new pixels are within range
-    #  This is why I'm sometimes shrinking things.  
-    if new_x < 1
-        new_x = 1
-    end
-    if new_y < 1
-        new_y = 1
-    end
-    if new_x > nx
-        new_x = nx
-    end
-    if new_y > ny
-        new_y = ny
-    end
-    
-    #  Now you can assign the pixels.
-    newImage[assignable_pixel_index] = globeImage[1, new_x, new_y]     #R
-    newImage[assignable_pixel_index + 1] = globeImage[2, new_x, new_y] #G
-    newImage[assignable_pixel_index + 2] = globeImage[3, new_x, new_y] #B
-    
-    assignable_pixel_index += 3
-    end
-    
-    #  Print out some progress.
-    if (y_index % 10 == 0)
-        completion_percentage = 100 *  y_index / ny
-        println(completion_percentage, "% complete ...")
-    end
-    
-    end
+        
+        #  Do just the y stuff here.  
+        φ = asin( (2 * y_index / ny)  - 1 )    
+        
+        for x_index = 1:nx
+            
+            #  At each loop, you want to apply the full transformation to get the new_x and new_y.
+            #  Then put the correct pixel value in each color.  
+            
+            
+            #  Assign longitude.
+            λ = (2*pi) * ((x_index / nx) - (1/2))
+            
+            newLocation = rotateByQuaternion(SphCoords(φ, λ), q)
+            newφ = newLocation.φ
+            newλ = newLocation.λ
+            
+            #  Adjust to ensure longitudes and latitudes are in the appropriate ranges.
+            if newλ > pi
+                newλ = newλ - (2*pi)
+            end
+            
+            
+            #  Obtain new pixel locations
+            #new_x = int(R*new_longitude)
+            #new_y = int(γ * R * sin(new_latitude))
+            new_x = int( nx * ((1/2) + (newλ/(2*pi))))
+            new_y = int( ny * (1/2) * (1+sin(newφ)))
+            
+            #  Ensure new pixels are within range
+            #  This is why I'm sometimes shrinking things.  
+            if new_x < 1
+                new_x = 1
+            end
+            if new_y < 1
+                new_y = 1
+            end
+            if new_x > nx
+                new_x = nx
+            end
+            if new_y > ny
+                new_y = ny
+            end
+            
+            #  Now you can assign the pixels.
+            newImage[assignable_pixel_index] = globeImage[1, new_x, new_y]     #R
+            newImage[assignable_pixel_index + 1] = globeImage[2, new_x, new_y] #G
+            newImage[assignable_pixel_index + 2] = globeImage[3, new_x, new_y] #B
+        
+            assignable_pixel_index += 3
+            end
+        
+        #  Print out some progress.
+        if (y_index % 10 == 0)
+            completion_percentage = 100 *  y_index / ny
+            println(completion_percentage, "% complete ...")
+        end
+        
+        end
     
     #newImageName = "/home/pfoley/map$theta_x.$theta_y.$theta_z.png"
     println("Completed image.  About to write the image to $newName...")
