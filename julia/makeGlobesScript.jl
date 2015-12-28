7e12235d73624517c0e8493fef81125c68ec945b
Patrick <patrick.egan.foley@gmail.com>
2015-12-28 14:32:54 -0700

    Never got Images or ImageView to work, due to tcl-tk nonsense.  Going python now.

--- julia/makeGlobesScript.jl
+++ julia/makeGlobesScript.jl
@@ -1,15 +1,17 @@
 #  This script does everything on a per-pixel basis.
 
 using Images
-include("/home/pfoley/globesRepository/julia/globesFunctions.jl")
+include("/Users/pfoley/globesRepository/julia/globesFunctions.jl")
 #globeImage = imread("/home/pfoley/globesRepository/earth-huge.png")
-globeImage = imread("/home/pfoley/globesRepository/peters-sat.jpg")
+globeImage = load("/Users/pfoley/globesRepository/input_images/peters-sat.jpg")
+
+globeImageData = float(reinterpret(Uint8, data(globeImage))[1:3, :, :])
 
 
 println("Read in the image ...")
 
-(nc, nx, ny) = size(globeImage)
-newImage = globeImage
+(nc, nx, ny) = size(globeImageData)
+newImage = globeImageData
 
 image_size = (nx, ny)
 
@@ -18,47 +20,26 @@ theta_y = pi/7
 theta_z = pi/3
 
 rotMat = coordinateRotationFromXYZ(theta_x, theta_y, theta_z)
-
+print("rotation matrix is")
+print(rotMat)
 
 
 image_index = 1
 current_pixel = 1
+
 for y_index = 1:ny
 for x_index = 1:nx
 for color_index = 1:nc
 
-#  Get image pixel index.
-#current_pixel = ny*(x_index-1) + y_index
-#new_j = newPixels[current_pixel, 1]
-#new_k = newPixels[current_pixel, 2]
 
 (new_j, new_k) = newPixelsFromPixels([x_index y_index], image_size, rotMat)
 
-#println("image index is    ", image_index)
-#println("new j is     ", new_j)
-#println("new k is    ", new_k)
-#println("current pixel is ", current_pixel)
-#println("x index is  ", x_index)
-#println("y index is  ", y_index)
-#println("Finished one image column...")
-
-
-#  I know this looks bizarre.  It has to be like this, 
-#  because you can access with three dimensions, but for some reason,
-#  you can only write by indexing with one dimension.
-newImage[image_index] = globeImage[color_index, new_j, new_k]
+newImage[image_index] = globeImageData[color_index, new_j, new_k]
 
 #image pixel index is c changes 1st, then x, then y.
 image_index += 1
 
 
-#println("image index is    ", image_index)
-#println("new j is     ", new_j)
-#println("new k is    ", new_k)
-#println("current pixel is ", current_pixel)
-#println("x index is  ", x_index)
-#println("y index is  ", y_index)
-#println("Finished one image column...")
 end
 end
 #  I guess I can do some calculations here and print a % done.
@@ -68,7 +49,12 @@ completion_percentage = 100 *  y_index / ny
 println(completion_percentage, "% complete ...")
 end
 
+print("New image is")
+print(newImage[:, 50:55, 100])
+print("Old image was")
+print(globeImageData[:, 50:55, 100])
 
+myimg = shareproperties(newImage, globeImage)
 
 println("Got all the new pixel values.  Now about to write the image.")
-imwrite(newImage, "/home/pfoley/newGlobe24.png")
+save("/Users/pfoley/newGlobeNewCode.png", myimg)
